9d0ec7f3dd88609f86fe38bde5fcfb5ecd79d26b
Gio d'Amelio <giodamelio@gmail.com>
2023-09-07 21:52:15 -0700

    Parse commands with Clap. Add some blank subcommands.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,63 @@
+use clap::{Parser, Subcommand};
+
+/// Alternate StreamDeck companion software
+#[derive(Debug, Parser)]
+#[command(author, version, about)]
+struct Cli {
+    #[clap(subcommand)]
+    command: Option<Command>,
+}
+
+#[derive(Debug, Subcommand)]
+enum Command {
+    /// Help troubleshoot system
+    #[command(arg_required_else_help = true)]
+    Debug {
+        #[clap(subcommand)]
+        command: DebugCommand,
+    },
+}
+
+#[derive(Debug, Subcommand)]
+enum DebugCommand {
+    /// Audio related tools
+    Audio {
+        #[clap(subcommand)]
+        command: AudioCommand
+    },
+}
+
+#[derive(Debug, Subcommand)]
+enum AudioCommand {
+    /// List audio devices
+    ListDevices,
+    /// List audio streams for a device
+    ListStreams {
+        device: String
+    },
+}
+
 fn main() {
-    println!("Hello, world!");
+    let cli = Cli::parse();
+    println!("{:#?}", cli);
+    match cli.command {
+        None => {
+            todo!("Start the main application");
+        }
+        Some(command) => {
+            match command {
+                Command::Debug { command } => match command {
+                    DebugCommand::Audio { command } => match command {
+                        AudioCommand::ListDevices => {
+                            todo!("List devices");
+                        }
+                        AudioCommand::ListStreams { device } => {
+                            todo!("Listing streams for device={}", device);
+                        }
+                    }
+                }
+            }
+        }
+    }
+
 }
