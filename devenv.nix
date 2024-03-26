a808037f52d618e75fd9b76c29139b7b25ef357b
Giovanni d'Amelio <gio@damelio.net>
2024-03-25 22:04:09 -0700

    Setup devenv for Rust and Nix

--- devenv.nix
+++ devenv.nix
@@ -1,28 +1,8 @@
-{ pkgs, ... }:
+{ pkgs, ... }: {
+  languages.nix.enable = true;
+  languages.rust.enable = true;
 
-{
-  # https://devenv.sh/basics/
-  env.GREET = "devenv";
-
-  # https://devenv.sh/packages/
-  packages = [ pkgs.git ];
-
-  # https://devenv.sh/scripts/
-  scripts.hello.exec = "echo hello from $GREET";
-
-  enterShell = ''
-    hello
-    git --version
-  '';
-
-  # https://devenv.sh/languages/
-  # languages.nix.enable = true;
-
-  # https://devenv.sh/pre-commit-hooks/
-  # pre-commit.hooks.shellcheck.enable = true;
-
-  # https://devenv.sh/processes/
-  # processes.ping.exec = "ping example.com";
-
-  # See full reference at https://devenv.sh/reference/options/
+  pre-commit.hooks.cargo-check.enable = true;
+  pre-commit.hooks.clippy.enable = true;
+  pre-commit.hooks.rustfmt.enable = true;
 }
