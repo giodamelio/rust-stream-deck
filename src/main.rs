812b5ebaa6390bcd918c9bee380431566598f710
Giovanni d'Amelio <gio@damelio.net>
2024-03-26 09:51:27 -0700

    Bevy Hello World

--- src/main.rs
+++ src/main.rs
@@ -1,26 +1,25 @@
-use elgato_streamdeck::StreamDeck;
+use bevy::{
+    app::{App, Update},
+    asset::AssetPlugin,
+    log::LogPlugin,
+    render::texture::ImagePlugin,
+    MinimalPlugins,
+};
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
-    // Create instance of HidApi
-    let hid = elgato_streamdeck::new_hidapi()?;
-
-    // List devices and unsafely take first one
-    let devices = elgato_streamdeck::list_devices(&hid);
-    let (kind, serial) = devices.first().ok_or("OH NO")?;
-    println!("{:?}, {:?}", kind, serial);
-
-    // Connect to the device
-    let device = StreamDeck::connect(&hid, *kind, &serial).expect("Failed to connect");
-
-    // Print out some info from the device
-    println!(
-        "Connected to '{}' with version '{}'",
-        device.serial_number()?,
-        device.firmware_version()?
-    );
-
-    // Set device brightness
-    device.set_brightness(100).unwrap();
+    App::new()
+        .add_plugins((
+            MinimalPlugins,
+            AssetPlugin::default(),
+            ImagePlugin::default(),
+            LogPlugin::default(),
+        ))
+        .add_systems(Update, log)
+        .run();
 
     Ok(())
+}
+
+fn log() {
+    println!("HELLO BEVY");
 }
