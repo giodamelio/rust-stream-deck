cb73fec3cacb44cad2000aa79ca943a850804a39
Giovanni d'Amelio <gio@damelio.net>
2024-03-26 12:50:11 -0700

    Get inputs flowing from the StreamDeck into Bevy

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,13 @@
 mod streamdeck;
 
 use bevy::{
-    app::{App, Update},
     asset::AssetPlugin,
+    prelude::{App, ButtonInput, Res, Update},
     render::texture::ImagePlugin,
     MinimalPlugins,
 };
 
-use crate::streamdeck::StreamDeckPlugin;
+use crate::streamdeck::{StreamDeckButton, StreamDeckPlugin};
 
 fn main() -> anyhow::Result<()> {
     pretty_env_logger::try_init()?;
@@ -19,12 +19,18 @@ fn main() -> anyhow::Result<()> {
             ImagePlugin::default(),
         ))
         .add_plugins(StreamDeckPlugin)
-        .add_systems(Update, log)
+        .add_systems(Update, log_buttons)
         .run();
 
     Ok(())
 }
 
-fn log() {
-    // info!("Hello Bevy!");
+fn log_buttons(button: Res<ButtonInput<StreamDeckButton>>) {
+    if button.just_pressed(StreamDeckButton::Button(0)) {
+        log::info!("Button 0 pressed");
+    }
+
+    if button.just_pressed(StreamDeckButton::Encoder(0)) {
+        log::info!("Encoder 0 pressed");
+    }
 }
--- src/streamdeck.rs
+++ src/streamdeck.rs
@@ -1,18 +1,99 @@
+use std::time::Duration;
+
 use anyhow::{anyhow, Result};
-use bevy::app::{Plugin, PreStartup};
-use elgato_streamdeck::StreamDeck;
-use log::debug;
+use bevy::{
+    prelude::{ButtonInput, Commands, Plugin, PreStartup, PreUpdate, Res, ResMut, Resource},
+    tasks::IoTaskPool,
+};
+use crossbeam_channel::{unbounded, Receiver, Sender};
+use elgato_streamdeck::{StreamDeck, StreamDeckInput};
+use tracing::{debug, trace};
 
 pub struct StreamDeckPlugin;
 
 impl Plugin for StreamDeckPlugin {
     fn build(&self, app: &mut bevy::prelude::App) {
-        app.add_systems(PreStartup, listener);
+        app.add_systems(PreStartup, listener)
+            .add_systems(PreUpdate, recieve_inputs)
+            .init_resource::<ButtonInput<StreamDeckButton>>();
     }
 }
 
-fn listener() {
-    let _deck = get_exactly_one_streamdeck();
+#[derive(Debug, Clone, Copy, Hash, PartialEq, Eq)]
+pub enum StreamDeckButton {
+    Button(i8),
+    Encoder(i8),
+}
+
+#[derive(Debug, Resource)]
+struct StreamDeckInputListener {
+    inputs_rx: Receiver<StreamDeckInput>,
+}
+
+// This system owns the connection to the StreamDeck
+// It communicates to the rest of the application via channels
+fn listener(mut commands: Commands) {
+    let (inputs_tx, inputs_rx) = unbounded::<StreamDeckInput>();
+
+    let taskpool = IoTaskPool::get();
+    taskpool.spawn(listener_task(inputs_tx)).detach();
+
+    commands.insert_resource(StreamDeckInputListener { inputs_rx });
+}
+
+#[tracing::instrument]
+async fn listener_task(inputs_tx: Sender<StreamDeckInput>) -> Result<()> {
+    let deck = get_exactly_one_streamdeck()?;
+
+    loop {
+        match deck.read_input(Some(Duration::from_millis(1)))? {
+            // Throw away no data events
+            StreamDeckInput::NoData => (),
+            input => inputs_tx.try_send(input)?,
+        };
+    }
+}
+
+// Convert input events from the StreamDeck to Bevy ButtonInput
+fn recieve_inputs(
+    input_listener: Res<StreamDeckInputListener>,
+    mut inputs: ResMut<ButtonInput<StreamDeckButton>>,
+) {
+    inputs.clear();
+
+    for input in input_listener.inputs_rx.try_iter() {
+        trace!("Recieved input: {:?}", input);
+
+        match input {
+            StreamDeckInput::ButtonStateChange(buttons) => {
+                for (index, state) in buttons.iter().enumerate() {
+                    let key = StreamDeckButton::Button(index as i8);
+                    match (*state, inputs.pressed(key)) {
+                        // If it is pressed and not already pressed, press it
+                        (true, false) => inputs.press(key),
+                        // If it is not pressed, and not already relased, release it
+                        (false, true) => inputs.release(key),
+                        // Otherwise the state stayed the same and we can ignore it
+                        _ => {}
+                    }
+                }
+            }
+            StreamDeckInput::EncoderStateChange(knobs) => {
+                for (index, state) in knobs.iter().enumerate() {
+                    let knob = StreamDeckButton::Encoder(index as i8);
+                    match (*state, inputs.pressed(knob)) {
+                        // If it is pressed and not already pressed, press it
+                        (true, false) => inputs.press(knob),
+                        // If it is not pressed, and not already relased, release it
+                        (false, true) => inputs.release(knob),
+                        // Otherwise the state stayed the same and we can ignore it
+                        _ => {}
+                    }
+                }
+            }
+            _ => {}
+        }
+    }
 }
 
 // Get the StreamDeck device
@@ -36,30 +117,3 @@ fn get_exactly_one_streamdeck() -> Result<StreamDeck> {
 
     Ok(device)
 }
-
-// +use elgato_streamdeck::StreamDeck;
-// +
-// +fn main() -> Result<(), Box<dyn std::error::Error>> {
-// +    // Create instance of HidApi
-// +    let hid = elgato_streamdeck::new_hidapi()?;
-// +
-// +    // List devices and unsafely take first one
-// +    let devices = elgato_streamdeck::list_devices(&hid);
-// +    let (kind, serial) = devices.first().ok_or("OH NO")?;
-// +    println!("{:?}, {:?}", kind, serial);
-// +
-// +    // Connect to the device
-// +    let device = StreamDeck::connect(&hid, *kind, &serial).expect("Failed to connect");
-// +
-// +    // Print out some info from the device
-// +    println!(
-// +        "Connected to '{}' with version '{}'",
-// +        device.serial_number()?,
-// +        device.firmware_version()?
-// +    );
-// +
-// +    // Set device brightness
-// +    device.set_brightness(100).unwrap();
-// +
-// +    Ok(())
-//  }
