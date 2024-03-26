f197f79b246849fec03234402cd4989cc9dba138
Giovanni d'Amelio <gio@damelio.net>
2024-03-25 21:48:49 -0700

    Fresh start

--- crates/legacy/src/cmd/mod.rs
+++
@@ -1,9 +0,0 @@
-mod debug_audio_listdevices;
-mod debug_audio_liststreams;
-mod debug_deck_listdevices;
-mod debug_deck_whiteout;
-
-pub use debug_audio_listdevices::run as debug_audio_listdevices;
-pub use debug_audio_liststreams::run as debug_audio_liststreams;
-pub use debug_deck_listdevices::run as debug_deck_listdevices;
-pub use debug_deck_whiteout::run as debug_deck_whiteout;
