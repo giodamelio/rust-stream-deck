fb5c389c2a0c80ddf19a11456ca39abf1b982446
Giovanni d'Amelio <gio@damelio.net>
2024-04-01 10:17:14 -0700

    Start building an async api for the StreamDeck Plus

--- src/main.rs
+++ src/main.rs
@@ -1,49 +1,13 @@
-use anyhow::Result;
-use async_hid::{AccessMode, DeviceInfo, SerialNumberExt};
-use futures_lite::StreamExt;
+mod streamdeck;
+
+use crate::streamdeck::StreamDeckPlus;
 
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
     pretty_env_logger::try_init()?;
 
-    let mut devices = DeviceInfo::enumerate().await?;
-    while let Some(device) = devices.next().await {
-        println!("Device: {:?}", device);
-        println!("Device: {:?}", device.serial_number());
-    }
-
-    let device = DeviceInfo::enumerate()
-        .await?
-        // StreamDeck Plus
-        .find(|info: &DeviceInfo| info.matches(12, 1, 4057, 132))
-        .await
-        .expect("Could not find device")
-        .open(AccessMode::ReadWrite)
-        .await?;
-
-    let mut buffer = [0u8; 32];
-    buffer[0] = 0x06;
-    let size = device.read_feature_report(&mut buffer).await?;
-    tracing::info!("Size: {}, Data: {:?}", size, &buffer[..size]);
-    tracing::info!("Serial?: {}", extract_string(&buffer[1..])?);
-
-    // Get Info
-    tracing::info!("Info: {:?}", device.info());
+    let mut deck = StreamDeckPlus::connect_exactly_one().await?;
+    tracing::info!("Serial Number: {}", deck.serial_number().await?);
 
     Ok(())
-}
-
-fn extract_string(bytes: &[u8]) -> Result<String> {
-    // Find the position of the last non-NUL byte
-    let last_non_nul_pos = bytes.iter().rposition(|&x| x != 0x00);
-
-    // Determine the slice up to the last non-NUL byte (or the whole slice if no NUL bytes)
-    let trimmed_slice = match last_non_nul_pos {
-        Some(pos) => &bytes[..=pos],
-        None => bytes,
-    };
-
-    // Convert the trimmed slice to a Vec<u8> because String::from_utf8 expects Vec<u8>
-    let trimmed_vec = trimmed_slice.to_vec();
-    Ok(String::from_utf8(trimmed_vec)?)
 }
--- src/streamdeck.rs
+++ src/streamdeck.rs
@@ -1,2 +1,44 @@
+use anyhow::Result;
+use async_hid::{AccessMode, Device, DeviceInfo};
+use futures_lite::StreamExt;
 
+pub struct StreamDeckPlus {
+    device: Device,
+}
 
+impl StreamDeckPlus {
+    pub async fn connect_exactly_one() -> Result<Self> {
+        let device = DeviceInfo::enumerate()
+            .await?
+            // StreamDeck Plus
+            .find(|info: &DeviceInfo| info.matches(12, 1, 4057, 132))
+            .await
+            .expect("Could not find device")
+            .open(AccessMode::ReadWrite)
+            .await?;
+
+        Ok(Self { device })
+    }
+
+    pub async fn serial_number(&mut self) -> Result<String> {
+        let mut buffer = [0u8; 32];
+        buffer[0] = 0x06;
+        let _size = self.device.read_feature_report(&mut buffer).await?;
+        extract_string(&buffer)
+    }
+}
+
+pub fn extract_string(bytes: &[u8]) -> Result<String> {
+    // Find the position of the last non-NUL byte
+    let last_non_nul_pos = bytes.iter().rposition(|&x| x != 0x00);
+
+    // Determine the slice up to the last non-NUL byte (or the whole slice if no NUL bytes)
+    let trimmed_slice = match last_non_nul_pos {
+        Some(pos) => &bytes[..=pos],
+        None => bytes,
+    };
+
+    // Convert the trimmed slice to a Vec<u8> because String::from_utf8 expects Vec<u8>
+    let trimmed_vec = trimmed_slice.to_vec();
+    Ok(String::from_utf8(trimmed_vec)?)
+}
